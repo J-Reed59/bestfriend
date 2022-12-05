f9377316fbe3661b89a21c21fade5c460da39933
Jackson Reed <reed.jackson327@gmail.com>
2022-12-05 13:10:29 -0500

    First weird commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,2 +1,4 @@
 first_name = input("what is your name? ")
-print(f"Hello {first_name}, I love you <3")
+print(f"Hello {first_name}")
+best = input("who is the best? ")
+print(f"NO! {best} is not the best, I am the best!")
